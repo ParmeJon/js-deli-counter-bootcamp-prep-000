9a08b8e3a1a049d698700f6c960fcc496bc3b8cd
Jonathan Chan <jonchan816@gmail.com>
2018-10-11 05:46:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function currentLine(currentLine) {
     for (var i = 0; i < currentLine.length; i++) {
      var linePosition = i + 1;
      var lineName = currentLine[i];
-     katzDeliLine.push(linePosition, lineName);
-     return `The line is currently: ${katzDeliLine}`
+     katzDeliLine.push(`${linePosition}. ${lineName}`);
     }
+     return `The line is currently: ${katzDeliLine}`
   }
 }
